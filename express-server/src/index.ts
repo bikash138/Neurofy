9060e5346161f526355776a1d2ed339909d7731e
Bikash Shaw <vshaw138@gmail.com>
2025-08-03 16:28:21 +0530

    init

---
+++ express-server/src/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("Hello from Server")
+})
+const data = {
+    message: "Hello from Express Server"
+}
+async function postData() {
+    const response = await axios.post('http://localhost:8000/ai-server', data)
+    console.log(response.data)
+}
+postData();
+
+
+app.listen(4000, () => {
+    console.log("Server is Running at PORT 4000")
+})
